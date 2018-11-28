07f71e4e0071074d89f50c1d3443a53373712c37
Ian Klupar <iklupar@nd.edu>
2018-11-28 10:20:55 -0500

    Homework 11

--- combine.sh
+++ combine.sh
@@ -1,6 +1,6 @@
 
-#run this script in a folder that has sporecoat fasta files. It will combine them all into one file
-# and then run the single file through muscle
+#run this script in a folder that has sporecoat (and transporter) fasta files. It will combine them all into one file
+# and then run the single file through muscle. For transporter files just change sporecoat* to transport*
 
 for a in sporecoat*
 do
